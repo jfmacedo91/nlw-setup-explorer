ab7c8022a971c0152699409c7fa00662fa2570dd
Jean Macedo <jfmacedo91@gmail.com>
2023-01-20 09:45:33 -0300

    fourth class completed

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const form = document.querySelector("#habits-form")
 const nlwSetup = new NLWSetup(form)
-
-const data = {
-  run: ["02-01", "02-06", "02-03"],
-  water: ["02-01", "02-02", "02-03", "02-04", "02-05", "02-06"],
-  food: ["02-05"],
-  journal: ["02-02", "02-06"],
-  takePills: ["02-04"]
-}
+const button = document.querySelector("header button")
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-BR").slice(0, -5)
+  if(nlwSetup.dayExists(today)) {
+    alert("Dia já cadastrado! ⛔")
+    return
+  }
+
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
